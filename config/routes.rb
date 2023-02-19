3a1810d27aa7c33d0bc30f36e1ed00ca3d06fc7c
EC2 Default User <124165252+itiayu23@users.noreply.github.com>
2023-02-19 13:54:15 +0000

    Book2-20

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: "homes#top"
 resources :books, only: [:index, :create, :show, :edit, :destroy, :update]
-resources :users, only: [:show, :edit, :index, :update]
+resources :users, only: [:show, :edit, :update]
 end
