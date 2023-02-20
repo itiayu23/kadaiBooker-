808cf9054a906e9ef87800d373e428fc54bcaf66
EC2 Default User <124165252+itiayu23@users.noreply.github.com>
2023-02-20 07:53:55 +0000

    Book2-valiOk

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,14 +10,18 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
   if @book.save
     flash[:notice] = "You have created book successfully."
-    redirect_to books_path(@book.id)
+    redirect_to book_path(@book.id)
   else
+    @books = Book.all
+    @user = current_user
     render :index
   end
   end
 
   def show
     @book = Book.find(params[:id])
+    @books = Book.new
+    @user = @book.user
   end
 
   def edit
