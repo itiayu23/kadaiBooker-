8dfb11b87336802e22016279b27d1f98bb41dbfd
EC2 Default User <124165252+itiayu23@users.noreply.github.com>
2023-03-01 03:01:20 +0000

    Follow oh2 Search1

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: "homes#top"
 get "home/about" => "homes#about"
-
+get "search" => "searches#search"
 resources :books do
   resource :favorites, only: [:create, :destroy]
   resources :book_comments, only: [:create, :destroy]
