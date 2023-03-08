e9c748d5f4bbe04f720342336b196422d6e09bbf
EC2 Default User <124165252+itiayu23@users.noreply.github.com>
2023-03-08 10:50:00 +0000

    DM OK

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,13 +1,13 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
     @room = Room.create
-    @current_entry = Entry.create(user_id: current_user.id, room_id: room.id)
+    @current_entry = Entry.create(user_id: current_user.id, room_id: @room.id)
     @another_entry = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id))
     redirect_to "/rooms/#{@room.id}"
   end
-  
+
   def index
     # ログインユーザー所属ルームID取得
     current_entries = current_user.entries
@@ -17,19 +17,20 @@ class RoomsController < ApplicationController
     end
     # 自分のroom_idでuser_idが自分じゃないのを取得
     @another_entries = Entry.where(room_id: my_room_id).where.not(user_id: current_user.id)
-    
+
   end
-  
+
   def show
     @room = Room.find(params[:id])
     if Entry.where(user_id: current_user.id, room_id: @room.id).present?
     @messages = @room.messages.all
     @message = Message.new
     @entries = @room.entries
+    @another_entry = @entries.where.not(user_id: current_user.id).first
     else
      redirect_back(fallback_location: room_path)
     end
-    
-    
+
+
   end
 end
