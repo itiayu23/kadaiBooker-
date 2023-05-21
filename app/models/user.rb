e621fee2d867567fee14da12a59ae540ee5efc10
EC2 Default User <124165252+itiayu23@users.noreply.github.com>
2023-05-21 08:01:24 +0000

    class add

--- app/models/user.rb
+++ app/models/user.rb
@@ -60,33 +60,33 @@ has_many :view_counts, dependent: :destroy
     def following?(user)
       followings.include?(user)
     end
-
-
-
-
+    
+    
+    
+    
     # 検索方法分岐
     def self.looks(search, word)
-
+      
       if search == "perfect_match"
         @user = User.where("name LIKE?", "#{word}")
-
+        
         elsif search == "forward_match"
           @user = User.where("name LIKE?","#{word}%")
-
+          
           elsif search == "backward_match"
             @user = User.where("name LIKE?", "%#{word}")
-
+            
             elsif search == "partial_match"
               @user = User.where("name LIKE?", "%#{word}%")
-
+              
             else
               @user = User.all
             end
           end
-
-
-
-
+              
+            
+        
+          
 
 
 
